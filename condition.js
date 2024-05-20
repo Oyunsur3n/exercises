a31da2288b78cdc6c6b2833deac0746d21dd7def
Oyunsuren Purevsuren <oyunsuren.p@nest.edu.mn>
2024-05-20 15:27:27 +0800

    first commit

---
+++ condition.js
@@ -0,0 +1,32 @@
+// easy
+// 1.Check if Number is Positive, Negative, or Zero
+// 2.Check Age for Voting
+// 3.Check if a number is odd or even
+// 4.Categorize Age Group (child, teenager, adult)
+// 5.Check Triangle Validity
+// 6.Check if variable is a number or not
+// 7.Find the largest of two number
+// 8.Check if a number is divisible by 5
+// 9.Check if a character is a vowel or a consonant
+// 10.Check if a number is within the range of 1 to 100
+// 11.Check if a password is strong (at least 8 characters long)
+
+// medium
+// 1.Find the largest of three number
+// 2.Check year is an Olympic year
+// 3.Determine the grade based on marks
+// 4.Амралтын өдөр эсвэл ажлын өдөр эсэхийг шалгаарай. 
+// 5.
+// 6.
+// 7.
+// 8.
+// 9.
+// 10.
+// 11.
+
+// medium <-> hard
+// 1.
+// 2.
+// 3.
+// 4.
+// 5.
---
+++ loop.js
@@ -0,0 +1,33 @@
+// easy
+// 1.Print Numbers 1 to 10
+// 2.Sum of First 10 Natural Numbers
+// 3.Print Even Numbers 1 to 20
+// 4.Print Odd Numbers 1 to 20
+// 5.Count Down from 10 to 1
+// 6.Print Elements Greater Than 10
+// 7.Reverse an Array
+// 8.Sum of Array Elements
+// 9.Print Array Elements at Even Indexes
+// 10.Calculate Average of Array Elements
+// 11.Given a string change the every second letter to an uppercase ‘Z’. Assume there are no space.
+
+// medium
+// 1.Print a Triangle of Stars
+// 2.Check Prime Number
+// 3.Count Vowels in a String
+// 4.Sum of Digits of a Number
+// 5.Calculate Power (toonii zereg)
+// 6.Calculate Factorial
+// 7.Find Maximum Element in an Array
+// 8.Find Minimum Element in an Array
+// 9.Sum of Odd Numbers in an Array
+// 10.Find Common Elements from Arrays
+// 11.Write a JavaScript program that displays the largest integer among two integers.
+
+// medium <-> hard
+// 1.Write a JavaScript conditional statement to find the largest 3 number from array.
+// 2.Iterate through all numbers from 1 to 45. Print the following: For multiples of 3 print “Fizz” , For multiples of 5 print “Buzz” , For multiples of 3 and 5 print “FizzBuzz”
+// 3.Given two arrays of integers. Add up each element in the same position and create a new array containing the sum of each pair. 
+// Assume both arrays are of the same length.
+// 4.Check Palindrome String
+// 5.
