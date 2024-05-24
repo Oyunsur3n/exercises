7e79265a0fa4b000dd10ab54b4f6cd7b33cdfa5a
Oyunsuren Purevsuren <oyunsuren.p@nest.edu.mn>
2024-05-24 16:07:26 +0800

    last

--- condition.js
+++ condition.js
@@ -9,50 +9,47 @@
 // 3.Check the given number is odd or even .
 // Өгөгдсөн тоог шалгаад тэгш тоо бол "even" , сондгой тоо бол "odd" гэж хэвлэнэ үү .
 
-// 7.Check if a given number is divisible by 5 .
+// 4.Check if a given number is divisible by 5 .
 // Өгөгдсөн тоо нь 5-д хуваагддаг үгүйг шалгана уу.
 
-// 9.Check if a given number is within the range of 1 to 100 .
+// 5.Check if a given number is within the range of 1 to 100 .
 // Өгөгдсөн тоо 1-ээс 100-н хооронд байвал "include" , байхгүй бол "exclude" гэж хэвлэнэ үү .
 
-// 10.Check if a password is strong (starong password is at least 8 characters long) .
+// 6.Check if a password is strong (starong password is at least 8 characters long) .
 // Пасспортыг хүчтэй үгүйг шалгаад хүчтэй бол "strong" , сул бол "weak" гэж хэвлэнэ үү. Багадаа 8 тэмдэгт агуулвал хүчтэй гэж тооцно .
 
 // medium
-// 4.Categorize age group (children, youth, adults , seniors) .
+// 7.Categorize age group (children, youth, adults , seniors) .
 // Өгөгдсөн нас нь 14-өөс доош бол "Children", 14-24 бол "Youth" , 24-64 бол "Adults" , 64-өөс дээш бол "Seniors" гэж хэвлэнэ үү .
 
-// 5.Check triangle validity . 
+// 8.Check triangle validity . 
 // Өгөгдсөн гурвалжны гурван талыг зөв эсэхийг шалгана уу .
 // for example : 
 //      a=5 b=6 c=7 / is valid 
 //      a=4 b=8 c=19 / is invalid 
 
-// 6.Find the largest of two number .
+// 9.Find the largest of two number .
 // 2 тооноос ихийг нь олж хэвлэнэ үү .
 
-// 8.Check if a given character is a vowel or a consonant .
+// 10.Check if a given character is a vowel or a consonant .
 // Өгөгдсөн үсгийг шалгаад эгшиг бол "vowel" , гийгүүлэгч бол "consonant" гэж хэвлэнэ үү .
 
-// 11.Find the largest of three number
-// 3 тооноос хамгийн ихийг нь олж хэвлэнэ үү .
-
-// 12.Check year is an Olympic year
+// 11.Check year is an Olympic year
 // Өгөгдсөн жил нь Олимпийн жил мөн бишийг шалгаад хэвлэнэ үү .
 
-// 13.Амралтын өдөр эсвэл ажлын өдөр эсэхийг шалгаарай. 
+// 12.Амралтын өдөр эсвэл ажлын өдөр эсэхийг шалгаарай. 
 // Өгөгдсөн өдөр ажлын өдөр бол "working day" , амралтын өдөр бол "weekend" гэж хэвлэнэ үү .
 // for example : 
 //      day = Thursday / working day  
 //      day = Saturday / weekend 
 
-// 14.Check the given number is not a prime number .
+// 13.Check the given number is not a prime number .
 // Өгөгдсөн тоог анхны тоо мөн бишийг шалгана уу . (1 болон өөртөө хуваагддаг тоог анхны тоо гэнэ)
 // example : 
 // number = 2 // true
 // number = 15 // false
 
-// 15.Өгөгдсөн сурагчийн дүнг A , B , C , D , F бүлэгт ангилж хэвлэнэ үү .
+// 14.Өгөгдсөн сурагчийн дүнг A , B , C , D , F бүлэгт ангилж хэвлэнэ үү .
 //    80-100, A
 //    70-89, B
 //    60-69, C
@@ -60,20 +57,11 @@
 //    0-49, F
 
 //hard
-// 16.Check if the season is Autumn, Winter, Spring or Summer.
+// 15.Check if the season is Autumn, Winter, Spring or Summer.
 // Өгөгдсөн сарыг шалгаад тохирох улирлыг хэвлэнэ үү .
 // for example : month = November / Autumn
 
-// 17.Яг одоогийн цагийг шалган өглөө бол "Good morning", өдөр бол "Good afternoon", орой бол "Good evening" гэж хэвлэнэ үү .
+// 16.Яг одоогийн цагийг шалган өглөө бол "Good morning", өдөр бол "Good afternoon", орой бол "Good evening" гэж хэвлэнэ үү .
 
-// 18.Цаг агаар температур , "sunny" эсвэл "rainy" гэж өгөгдөх ба бороотой бол "Don't forget your umbrella!" , нартай бол температур нь 30-аас их бол "It's a hot day!" , 20-30 градус бол "It's a warm day" , 20-оос бага бол "It's a bit cool today" гэж хэвлэнэ үү .
+// 17.Цаг агаар температур , "sunny" эсвэл "rainy" гэж өгөгдөх ба бороотой бол "Don't forget your umbrella!" , нартай бол температур нь 30-аас их бол "It's a hot day!" , 20-30 градус бол "It's a warm day" , 20-оос бага бол "It's a bit cool today" гэж хэвлэнэ үү .
 // for example : weather = "sunny" temperature = 25 / It's a warm day.
-
-// 19.yearOfBirth гэсэн хувьсагчид төрсөн оныг оноож өгнө үү. Тухайн оноос тухайн хүний насыг нь бодож гаргаад үр дүнгээ old гэсэн хувьсагчид оноо. Насанд нь харгалзах утгыг хэвлэ.
-// 0 <= old <= 1 үед "Infant"  
-// 1 < old <= 3 үед "Toddler" 
-// 3 < old <= 5 үед "Preschool"
-// 5 < old <=12 үед "Gradeschooler"
-// 12 <old <= 18 үед "Teen"
-// 18 < old <=21 үед "Young adult"
-// 21 < old үед "Adult"
--- loop.js
+++ loop.js
@@ -11,21 +11,13 @@
 // 1 - 20 хоорондох тэгш тоонуудыг хэвлэнэ үү .
 // for example : 2, 4, 6, ... , 20
 
-// 4.Print odd numbers 0 to 15 .
-// 0 - 15 хоорондох сондгой тоонуудыг хэвлэнэ үү .
-// for example : 1, 3, 5, ... , 15
-
-// 5.Print numbers from 10 to 1 .
-// 10 -> 1 хүртэл хэвлэнэ үү .
-// for example : 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
-
 // medium
-// 6.Given string change the every second letter to an ‘Z’. Assume there are no space.
+// 4.Given string change the every second letter to an ‘Z’. Assume there are no space.
 // Өгөгдсөн string-н 2дох үсэг болгоныг Z үсгээр солин бичнэ үү . Өгөгдсөн string дундаа зайгүй гэж үзнэ .
 // input : helloworld
 // for example : hZlZoZoZlZ
 
-// 7.Print a square of stars (*) .
+// 5.Print a square of stars (*) .
 // (*) Од ашиглан дөрвөлжинг хэвлэнэ үү .
 // for example :
 // *****
@@ -33,67 +25,57 @@
 // *****
 // *****
 
-// 8.Count vowels in given string .
-// Өгөгдсөн string-н эгшгүүдийг тоолно уу .
-// input : ilovecode
-// for example : 5
-
-// 9.Sum of digits of given number .
+// 6.Sum of digits of given number .
 // Өгөгдсөн тооны цифрүүдийн нийлбэрийг олно уу.
 // input : 7894560
 // for example : 39
 
-// 10.Calculate given number to power .
+// 7.Calculate given number to power .
 // Өгөгдсөн тоог тухайн зэрэгт дэвшүүлнэ үү .
 // input : number = 6  degree=10
 // for example : 60466176
 
-// 11.Calculate factorial of given number.
+// 8.Calculate factorial of given number.
 // Өгөгдсөн тооны факторалыг ол . (1 - ээс тухайн тоо хүртэлх тооны үржвэр)
 // input : 8
 // for example : 40320
 
-// 12.Calculate average of numbers from 12 to 36 .
+// 9.Calculate average of numbers from 12 to 36 .
 // 12 -> 36 хүртэлх тоонуудын дунджийг ол .
 // for example : 24
 
-// 13.Sum of odd and even numbers from 11 to 50 .
+// 10.Sum of odd and even numbers from 11 to 50 .
 // 11 -> 50 хүртэлх тоонуудын тэгш болон содгой тоонуудын нийлбэрийг ол .
 // for example : sumOdd = 600, sumEven = 620
 
-// 14.Iterate through all numbers from 1 to 45. Print the following: For multiples of 3 print “Fizz” , For multiples of 5 print “Buzz” , For multiples of 3 and 5 print “FizzBuzz”
+// 11.Iterate through all numbers from 1 to 45. Print the following: For multiples of 3 print “Fizz” , For multiples of 5 print “Buzz” , For multiples of 3 and 5 print “FizzBuzz”
 // 1 - 45 хүртэлх тоонуудын давталт гүйлгэж 3д хуваагдаж байвал “Fizz” , 5д хуваагдаж байвал “Buzz” , 3 болон 5д хуваагдаж байвал “FizzBuzz” гэж хэвлэнэ үү .
 // for example : number = 6 // Fizz , number = 8 // empty , number = 15 // FizzBuzz
 
-// 15.Find the largest overlap of digits in the numbers from 123 to 200 .
+// 12.Find the largest overlap of digits in the numbers from 123 to 200 .
 // 123 - 200 хүртэлх тоонуудын цифр хамгийн их давхацсаныг олно уу .
 // for example : 0 = 9 times , 2 = 15 times , 3 = 18 times
 
-// 16.Iterate through all numbers from 1 to 5 and from 20 to 16 . Add the corresponding value .
+// 13.Iterate through all numbers from 1 to 5 and from 20 to 16 . Add the corresponding value .
 // 1-5 , 20-16 гэсэн тоонуудаар давталт гүйлгэж харгалзах утгатай нь нэмнэ үү .
 // 1 , 2 , 3 , 4 , 5
 // 20 , 19 , 18 , 17 , 16
 // for example : 21 , 21 , 21 , 21 , 21
 
-// 17.Check the fifth power of given number is greater than 10000 .
+// 14.Check the fifth power of given number is greater than 10000 .
 // Тооны таван зэрэг нь 10000-аас дээш эсэхийг шалгана уу .
 // for example :
 //      5 * 5 * 5 * 5 * 5 = 3125 // it is less than 10000
 //      8 * 8 * 8 * 8 * 8 = 32768 // it is greater than 10000
 
 // hard
-// 18.Check the given string is not a palindrome .
-// Өгөгдсөн string рalindrome үг мөн бишийг шалгаж мөн бол it is palindrome word биш бол it isn't palindrome word гэж хэвлэнэ үү.
-// input : repaper // it is palindrome
-// input : world // it isn't palindrome word
-
-// 19.N тоо өгөхөд NxN хэмжээтэй шатрын хөлөг буцаах .
+// 15.N тоо өгөхөд NxN хэмжээтэй шатрын хөлөг буцаах .
 // Гаралт:
 // _#_
 // #_#
 // _#_
 
-// 20.Print a Pyramid of Stars (*) .
+// 16.Print a Pyramid of Stars (*) .
 // (*) Од ашиглан пирамидыг хэвлэнэ үү .
 // for example :
 //    *
@@ -101,4 +83,4 @@
 //  *****
 // *******
 
-// 21.Фибоначчийн тоон дараалал  a1 = a2 = 1 ба a(n+2)=a(n+1)+ an  гэж тодорхойлогддог.  Тус дарааллын 20 дох гишүүнийг 5-д ол.
+// 17.Фибоначчийн тоон дараалал  a1 = a2 = 1 ба a(n+2)=a(n+1)+ an  гэж тодорхойлогддог.  Тус дарааллын 20 дох гишүүнийг 5-д ол.
